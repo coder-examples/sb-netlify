de4ca30fcb1e871b03da3eec39e6dbd81807c054
Aayush Shah <visionmoneymantra.web@gmail.com>
2020-04-08 11:40:17 +0530

    Add build

---
+++ build/precache-manifest.95839fb568a3babeb3627b5c25015a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f24ae5725b234426bb88c2d905a4ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a701ad388ca13549f53c",
+    "url": "./static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3eeace5a7705f2ddfc92",
+    "url": "./static/css/main.8967b8f4.chunk.css"
+  },
+  {
+    "revision": "a701ad388ca13549f53c",
+    "url": "./static/js/2.28357ad9.chunk.js"
+  },
+  {
+    "revision": "3eeace5a7705f2ddfc92",
+    "url": "./static/js/main.08b0071d.chunk.js"
+  },
+  {
+    "revision": "cd91da34cfd62d004fe9",
+    "url": "./static/js/runtime~main.ce511409.js"
+  }
+]);
